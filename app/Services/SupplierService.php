92605e286fab3c134e7191c61f3a6a8a5bc73b37
Andar <50786504+andaaree@users.noreply.github.com>
2026-04-10 09:19:54 +0700

    refactor(supplier) : change feedback handler

--- app/Services/SupplierService.php
+++ app/Services/SupplierService.php
@@ -3,10 +3,10 @@ namespace App\Services;
 
 use App\Models\Supplier;
 use App\Contracts\SuppliersInterface;
+use App\Traits\FeedbackHandler;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use App\Traits\GlobalWarn;
 class SupplierService implements SuppliersInterface{
-    use GlobalWarn;
+    use FeedbackHandler;
 
     public function paginate(int $perPage = 10):LengthAwarePaginator {
         return Supplier::query()
@@ -19,7 +19,7 @@ class SupplierService implements SuppliersInterface{
         try {
             return Supplier::create($data);
         } catch (\Exception $th) {
-            return $this->message('error',$th->getMessage());
+            return $this->err(Supplier::class,$th);
         }
     }
 
@@ -27,20 +27,20 @@ class SupplierService implements SuppliersInterface{
         try {
             $supplier->update($data);
             return $supplier;
-        } catch (\Exception $th) {
-            return $this->message('error',$th->getMessage());
+            } catch (\Exception $th) {
+                return $this->err(Supplier::class,$th);
+            }
         }
-    }
 
     public function delete(Supplier $supplier) {
-        try {
-            if ($supplier->layups()->exists()) {
-                return $this->message('error','Supplier masih memiliki layup');
-            }
-            $supplier->delete();
-            return $this->message('success','Supplier berhasil dihapus');
-        } catch (\Exception $e) {
-            return $this->message('error',$e->getMessage());
+      try {
+        if ($supplier->layups()->exists()) {
+          return $this->message($supplier,null,'Supplier  layup');
         }
+        $supplier->delete();
+        return $this->message($supplier,null,'Supplier deleted successfully');
+      } catch (\Exception $th) {
+        return $this->err(Supplier::class,$th);
+      }
     }
 }
